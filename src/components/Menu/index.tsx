84c1801de98e7676f7e86f4fe93f8c22da08f14a
Mayro <deehmyller11@gmail.com>
2022-10-04 15:31:04 -0300

    tarefa: adiciona icones e estilo para o menu em mobile first com testes de acessibilidade

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,8 +1,30 @@
+import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
+import { Search as SearchIcon } from '@styled-icons/material/Search'
+import { Menu2 as MenuIcon } from '@styled-icons/remix-line/Menu2'
+
+import Logo from '../Logo'
+
 import * as S from './styles'
 
 const Menu = () => (
   <S.Wrapper>
-    <h1>Menu</h1>
+    <S.IconWrapper>
+      <MenuIcon aria-label="Open Menu" />
+    </S.IconWrapper>
+
+    <S.LogoWrapper>
+      <Logo hideOnMobile />
+    </S.LogoWrapper>
+
+    <S.MenuGroup>
+      <S.IconWrapper>
+        <SearchIcon aria-label="Search" />
+      </S.IconWrapper>
+
+      <S.IconWrapper>
+        <ShoppingCartIcon aria-label="Open Shopping Cart" />
+      </S.IconWrapper>
+    </S.MenuGroup>
   </S.Wrapper>
 )
 
