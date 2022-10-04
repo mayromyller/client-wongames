f349af71217e7d66aafe56e5b25b7165cc56601d
Mayro <deehmyller11@gmail.com>
2022-10-04 16:02:41 -0300

    tarefa: adiciona lógica de abrir e fechar menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import { Search as SearchIcon } from '@styled-icons/material/Search'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-line/Menu2'
 
@@ -6,26 +9,36 @@ import Logo from '../Logo'
 
 import * as S from './styles'
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </S.IconWrapper>
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
 
-    <S.LogoWrapper>
-      <Logo hideOnMobile />
-    </S.LogoWrapper>
-
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <SearchIcon aria-label="Search" />
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open Menu" />
       </S.IconWrapper>
 
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="Open Shopping Cart" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-)
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <S.IconWrapper onClick={() => setIsOpen(false)}>
+          <CloseIcon aria-label="close menu" />
+        </S.IconWrapper>
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
