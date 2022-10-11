042966f56a86b1a24f48d81580de9c1ed1a8278e
Mayro <deehmyller11@gmail.com>
2022-10-10 23:49:04 -0300

    tarefa: adiciona estilo ao Footer e atualiza testes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,89 @@
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+import Link from 'next/link'
+
 import * as S from './styles'
 
 const Footer = () => (
   <S.Wrapper>
-    <h1>Footer</h1>
+    <Logo color="black" />
+
+    <S.Content>
+      <S.Column>
+        <Heading lineColors="secondary" lineBottom size="small" color="black">
+          Contacts
+        </Heading>
+
+        <a href="mailto:sac@wongames.com">sac@wongames.com</a>
+      </S.Column>
+
+      <S.Column>
+        <Heading lineColors="secondary" lineBottom size="small" color="black">
+          Follow US
+        </Heading>
+
+        <nav aria-labelledby="social media">
+          <a
+            href="http://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="http://www.twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="http://www.youtube.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            YouTube
+          </a>
+          <a
+            href="http://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading lineColors="secondary" lineBottom size="small" color="black">
+          Links
+        </Heading>
+
+        <nav aria-labelledby="footer resources">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/games">
+            <a>Store</a>
+          </Link>
+          <Link href="/search">
+            <a>Search</a>
+          </Link>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading lineColors="secondary" lineBottom size="small" color="black">
+          Location
+        </Heading>
+
+        <span>Rua das flores</span>
+        <span>527 - 89020330</span>
+        <span>Ceará, Brasil</span>
+      </S.Column>
+    </S.Content>
+
+    <S.Copyright>Won Games 2022 © Todos os Direitos Reservados</S.Copyright>
   </S.Wrapper>
 )
 
