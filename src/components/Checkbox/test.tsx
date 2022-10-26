cf5d3a9137d820047a05f8b74c6ee9f844ebb2cc
Mayro <deehmyller11@gmail.com>
2022-10-25 21:07:35 -0300

    tarefa: adiciona estrutura inicial do component Checkbox com testes

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render with label', () => {
+    render(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+  })
+
+  it('should render without label', () => {
+    render(<Checkbox />)
+
+    expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+  })
+})
