47b2bed47acd8fe9538ef57444f7564361913afc
Mayro <deehmyller11@gmail.com>
2022-11-21 17:03:28 -0300

    tarefa: adiciona metodo de onChange

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,24 +1,46 @@
+import { InputHTMLAttributes, useState } from 'react'
 import * as S from './styles'
 
 export type CheckboxProps = {
   label?: string
   labelFor?: string
   labelColor?: 'white' | 'black'
-}
+
+  onCheck?: (status: boolean) => void
+} & InputHTMLAttributes<HTMLInputElement>
 
 const Checkbox = ({
   label,
   labelFor = '',
-  labelColor = 'white'
-}: CheckboxProps) => (
-  <S.Wrapper>
-    <input type="checkbox" id={labelFor} />
-    {!!label && (
-      <S.Label htmlFor={labelFor} labelColor={labelColor}>
-        {label}
-      </S.Label>
-    )}
-  </S.Wrapper>
-)
+  labelColor = 'white',
+  onCheck
+}: CheckboxProps) => {
+  const [checked, setChecked] = useState(false)
+
+  function onChange() {
+    const status = !checked
+    setChecked(status)
+
+    if (onCheck) {
+      onCheck(status)
+    }
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        type="checkbox"
+        id={labelFor}
+        onChange={onChange}
+        checked={checked}
+      />
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default Checkbox
--- src/components/Checkbox/styles.ts
+++ src/components/Checkbox/styles.ts
@@ -2,10 +2,58 @@ import styled, { css } from 'styled-components'
 
 import { CheckboxProps } from '.'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
 
 export const Label = styled.label<Pick<CheckboxProps, 'labelColor'>>`
   ${({ theme, labelColor }) => css`
     color: ${theme.colors[labelColor!]};
+    padding-left: ${theme.spacings.xxsmall};
+    line-height: 1;
+  `}
+`
+
+export const Input = styled.input`
+  ${({ theme }) => css`
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    appearance: none;
+    width: 1.8rem;
+    height: 1.8rem;
+    border: 0.2rem solid ${theme.colors.darkGray};
+    border-radius: 0.2.rem;
+    transition: background border 0.1s ease-in-out;
+    position: relative;
+    outline: none;
+
+    &:before {
+      content: '';
+      width: 0.6rem;
+      height: 0.9rem;
+      border: 0.2rem solid ${theme.colors.white};
+      border-top: 0;
+      border-left: 0;
+      transform: rotate(45deg);
+      position: absolute;
+      top: 0.1rem;
+      transition: 0.1s ease-in-out;
+    }
+
+    &:focus {
+      box-shadow: 0 0 0.5rem ${theme.colors.primary};
+    }
+
+    &:checked {
+      border-color: ${theme.colors.primary};
+      background: ${theme.colors.primary};
+
+      &:before {
+        opacity: 1;
+      }
+    }
   `}
 `
