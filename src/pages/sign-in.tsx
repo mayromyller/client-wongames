cf2f63ab68052b00b38df40be6a95c0fe2ccfbe7
Mayro <deehmyller11@gmail.com>
2022-12-16 11:14:59 -0300

    tarefa: adiciona os componentes de form para as respectivas págincas

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,5 +1,10 @@
 import Auth from 'templates/Auth'
+import FormSignIn from 'components/FormSignIn'
 
 export default function SignIn() {
-  return <Auth title="Sign In">text</Auth>
+  return (
+    <Auth title="Entrar">
+      <FormSignIn />
+    </Auth>
+  )
 }
